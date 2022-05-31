ef07f55af94e32a9d4b80688b14ccd38412557d4
Narek <narekazibekyan5@gmail.com>
2022-05-31 18:17:53 +0400

    Numbers > 0

---
+++ Task_41/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите числа через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0) count++;
+}
+
+Console.WriteLine($"Ваши числа: {String.Join(", ", array)}.");
+Console.WriteLine($"Количество чисел больше 0 = {count}.");
