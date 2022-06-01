e6b5c948a099b6b11074969a3d8d774c9680b1c8
Narek <narekazibekyan5@gmail.com>
2022-06-01 14:52:25 +0400

    Nums > 0

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -6,11 +6,17 @@
 Console.Write("Введите числа через пробел: ");
 int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-int count = 0;
-for (int i = 0; i < array.Length; i++)
+
+int NumsMore0(int[] array)
 {
-    if (array[i] > 0) count++;
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+
+    return count;
 }
 
 Console.WriteLine($"Ваши числа: {String.Join(", ", array)}.");
-Console.WriteLine($"Количество чисел больше 0 = {count}.");
+NumsMore0(array);
